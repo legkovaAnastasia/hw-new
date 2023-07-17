13cf20ac6c8b96ad5605d78e91adbb1a7f940074
anastasia <legkova.anastasia127@gmail.com>
2023-07-17 14:44:29 +0300

    hw-4

--- src/HW/4/HW4.tsx
+++ src/HW/4/HW4.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import {ChangeEvent, useState} from 'react';
 import s from './HW4.module.css';
 import { Button } from './Button';
 import { Input } from './Input';
@@ -21,30 +21,33 @@ export const HW4 = () => {
     setTexts([currentText, ...texts]);
     setCurrentText('');
   };
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setCurrentText(event.currentTarget.value); ////сетала ивент
+  };
 
   return (
     <div id={'hw04'}>
-      {/*{currentText ? (*/}
-      {/*  <h1 id={'hw04-text'}>{currentText}</h1>*/}
-      {/*) : (*/}
-      {/*  <h1 id={'hw04-default-text'}>Здесь появится новое дело</h1>*/}
-      {/*)}*/}
+      {currentText ? (
+        <h1 id={'hw04-text'}>{currentText}</h1>
+      ) : (
+        <h1 id={'hw04-default-text'}>Здесь появится новое дело</h1>
+      )}
 
-      {/*<Input НЕ ХВАТАЕТ АРГУМЕНТОВ />*/}
+      <Input currentText={currentText} setCurrentText={setCurrentText}/>
 
-      {/*<Button НЕ ХВАТАЕТ АРГУМЕНТОВ />*/}
+      <Button name={'сохранить'} callBack={handleSave} />
 
-      {/*<h1 style={{ marginTop: '50px' }}>СПИСОК ДЕЛ НА ДЕНЬ:</h1>*/}
+      <h1 style={{ marginTop: '50px' }}>СПИСОК ДЕЛ НА ДЕНЬ:</h1>
 
-      {/*<ol id={'hw04-tasks'}>*/}
-      {/*  {texts.map((el, index) => {*/}
-      {/*    return (*/}
-      {/*      <li key={index} id={`hw04-task-${index}`} className={ЕСЛИ ЧЕТН ? S.chetNechet : ''}>*/}
-      {/*        {el}*/}
-      {/*      </li>*/}
-      {/*    );*/}
-      {/*  })}*/}
-      {/*</ol>*/}
+      <ol id={'hw04-tasks'}>
+        {texts.map((el, index) => {
+          return (
+            <li key={index} id={`hw04-task-${index}`} className={index%2 ? s.chetNechet : ''}>
+              {el}
+            </li>
+          );
+        })}
+      </ol>
     </div>
   );
 };
